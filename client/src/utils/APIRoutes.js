5aa390fd2018d2a0686df322749c6934fd76a602
Priyanshu Gaur <gaur.priyanshu169@gmail.com>
2024-05-17 17:17:31 +0530

    ft

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://matchorderproject.onrender.com/";
+export const host = "https://matchorderproject.onrender.com";
 export const pending_orders = `${host}/po/pending-order`;
 export const completed_orders = `${host}/co/complete-order`;
 export const search_seller_data = `${host}/or/create-order`; // Define the route path for searching seller data
