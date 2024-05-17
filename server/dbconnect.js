d7d66f9df92b8507a94cb18f026e6cc54ae74656
Priyanshu Gaur <gaur.priyanshu169@gmail.com>
2024-05-17 16:26:10 +0530

    second

--- server/dbconnect.js
+++ server/dbconnect.js
@@ -1,10 +1,30 @@
+// const mysql = require('mysql');
+
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'Jaipur@19',
+//     database: `order_matching_system`
+//  });
+ 
+//  connection.connect((err) => {
+//     if (err) {
+//         console.error('Error connecting to MySQL: ' + err.stack);
+//         return;
+//     }
+//     console.log('Connected to MySQL as id ' + connection.threadId);
+//  });
+
+
+//  module.exports = connection;
+
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Jaipur@19',
-    database: `order_matching_system`
+    host: 'bvmlqvwmx0ftv2aqkff0-mysql.services.clever-cloud.com',
+    user: 'udpc9pjnoik2ynwc',
+    password: 'eMZhdKR0ixw17vYLHKYz',
+    database: `bvmlqvwmx0ftv2aqkff0`
  });
  
  connection.connect((err) => {
