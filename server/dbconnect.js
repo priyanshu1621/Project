4d53f9145db509bb57c59f62160facd3f77ea520
Priyanshu Gaur <82864666+priyanshu1621@users.noreply.github.com>
2024-05-18 11:34:30 +0530

    Update dbconnect.js

--- server/dbconnect.js
+++ server/dbconnect.js
@@ -1,22 +1,4 @@
-// const mysql = require('mysql');
 
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'Jaipur@19',
-//     database: `order_matching_system`
-//  });
- 
-//  connection.connect((err) => {
-//     if (err) {
-//         console.error('Error connecting to MySQL: ' + err.stack);
-//         return;
-//     }
-//     console.log('Connected to MySQL as id ' + connection.threadId);
-//  });
-
-
-//  module.exports = connection;
 
 const mysql = require('mysql');
 
